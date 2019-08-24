c212e1487c29ee0c32cbf5e3b226eb9c08e8fb1b
Aria Bagas <aariabagass@gmail.com>
2019-08-24 23:56:10 +0700

    JS Task 27
    conversion join

---
+++ tugas27.js
@@ -0,0 +1,14 @@
+var joined = () => {
+  a = 'No'
+  b = ' rumah saya '
+  c = 30
+  d = 'A'
+  e = 'Jalan pangeran Tirtayasa No '
+  f = 70
+  newC = String(c);
+  newF = String(f);
+  console.log(a + b + newC + d);
+  console.log(e + newF);
+  return joined;
+}
+joined()
